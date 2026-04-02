8cdbffbd6269ff7fd9c808f45aa01bcaf80d4971
Jakob Schlichting (P227564) <Jakob.Schlichting@tk.de>
2026-04-02 14:15:21 +0200

    fix: migrate LSP setup to Neovim 0.11 APIs

--- lua/schlichting/plugin/lsp.lua
+++ lua/schlichting/plugin/lsp.lua
@@ -1,128 +1,149 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    "neovim/nvim-lspconfig",
     dependencies = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        -- {'williamboman/mason.nvim'},           -- Optional
-        -- {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "L3MON4D3/LuaSnip" },
     },
     config = function()
         local cmp = require("cmp")
-        local lsp = require("lsp-zero")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
-
-        lsp.preset("recommended")
-
-        lsp.set_sign_icons({
-            error = "",
-            warn = "",
-            hint = "",
-            info = "",
-        })
-
-        lsp.setup_nvim_cmp({
-            mapping = cmp_mappings
-        })
-
-        lsp.on_attach(function(_client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
-
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>of", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
-
-        require("lspconfig").gopls.setup({})
-        require("lspconfig").hyprls.setup({})
-        require("lspconfig").pylsp.setup({
-            settings = {
-                pylsp = {
-                    plugins = {
-                        pycodestyle = {
-                            ignore = {'W391'},
-                            maxLineLength = 100
-                        }
-                    }
-                }
-            }
-        })
-        require("lspconfig").ts_ls.setup({})
-        require("lspconfig").lua_ls.setup({
-            on_init = function(client)
-                if client.workspace_folders then
-                    local path = client.workspace_folders[1].name
-                    if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
-                        return
-                    end
-                end
-
-                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                    runtime = {
-                        version = 'LuaJIT'
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME
-                        }
-                    }
-                })
-            end,
-            settings = {
-                Lua = {}
-            }
-        })
-        require("lspconfig").yamlls.setup({})
-        require("lspconfig").marksman.setup({})
-        require("lspconfig").dockerls.setup({})
-        require("lspconfig").bashls.setup({})
-        require("lspconfig").markdown_oxide.setup({})
-        require("lspconfig").buf_ls.setup({})
-        require("lspconfig").cmake.setup({})
-        require("lspconfig").docker_compose_language_service.setup({})
-        require("lspconfig").terraform_lsp.setup({})
-        require("lspconfig").ansiblels.setup({})
-        require("lspconfig").html.setup({})
-        require("lspconfig").htmx.setup({})
-        require("lspconfig").templ.setup({})
-        require("lspconfig").nixd.setup({})
-        require("lspconfig").ols.setup({})
-        require("lspconfig").lspconfig.helm_ls.setup {
-            settings = {
-                ['helm-ls'] = {
-                    yamlls = {
-                        path = "yaml-language-server",
-                    }
-                }
-            }
-        }
-
-        lsp.setup()
-
-        -- enable diagnostics
         vim.diagnostic.config({
             virtual_text = true,
             signs = true,
         })
-    end
+
+        local signs = {
+            Error = "E",
+            Warn = "W",
+            Hint = "H",
+            Info = "I",
+        }
+
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            }),
+        })
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+                local opts = { buffer = args.buf, remap = false }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+            end,
+        })
+
+        local default_servers = {
+            "gopls",
+            "hyprls",
+            "ts_ls",
+            "yamlls",
+            "marksman",
+            "dockerls",
+            "bashls",
+            "markdown_oxide",
+            "buf_ls",
+            "cmake",
+            "docker_compose_language_service",
+            "terraformls",
+            "ansiblels",
+            "html",
+            "htmx",
+            "templ",
+            "nixd",
+            "ols",
+        }
+
+        for _, server in ipairs(default_servers) do
+            vim.lsp.config(server, {
+                capabilities = capabilities,
+            })
+            vim.lsp.enable(server)
+        end
+
+        vim.lsp.config("pylsp", {
+            capabilities = capabilities,
+            settings = {
+                pylsp = {
+                    plugins = {
+                        pycodestyle = {
+                            ignore = { "W391" },
+                            maxLineLength = 100,
+                        },
+                    },
+                },
+            },
+        })
+        vim.lsp.enable("pylsp")
+
+        vim.lsp.config("lua_ls", {
+            capabilities = capabilities,
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if path ~= vim.fn.stdpath("config") and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")) then
+                        return
+                    end
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                        },
+                    },
+                })
+            end,
+            settings = {
+                Lua = {},
+            },
+        })
+        vim.lsp.enable("lua_ls")
+
+        local ok = pcall(vim.lsp.config, "helm_ls", {
+            capabilities = capabilities,
+            settings = {
+                ["helm-ls"] = {
+                    yamlls = {
+                        path = "yaml-language-server",
+                    },
+                },
+            },
+        })
+        if ok then
+            vim.lsp.enable("helm_ls")
+        end
+    end,
 }
